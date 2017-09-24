567c33cd1d298d8a877943e4329077acdf87fc45
Tobias S. Sørensen <Tss3331@hotmail.com>
2017-09-24 12:10:26 +0200

    Har lavet metoden opret studenter og find studenter.

---
+++ WCFStudent/WCFStudent/Service1.svc.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFStudent
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+	// NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+	public class Service1 : IService1
+	{
+
+		static readonly List<Student> Studenterliste = new List<Student>();
+		public string Addstudent(int id, string navn, string efternavn, int alder)
+		{
+			var student = new Student(id, navn, efternavn, alder);
+			Studenterliste.Add(student);
+			return student.ToString();
+		}
+
+
+		public Student FindStudent(int ID)
+		{
+			var selected = Studenterliste.FirstOrDefault(x => x.Id == ID);
+
+			return selected;
+		}
+
+
+		public string EditStudent()
+		{
+			throw new NotImplementedException();
+		}
+
+	
+		public string GetAllStudent()
+		{
+			throw new NotImplementedException();
+		}
+
+		public string GetData(int value)
+		{
+			return string.Format("You entered: {0}", value);
+		}
+
+		public CompositeType GetDataUsingDataContract(CompositeType composite)
+		{
+			if (composite == null)
+			{
+				throw new ArgumentNullException("composite");
+			}
+			if (composite.BoolValue)
+			{
+				composite.StringValue += "Suffix";
+			}
+			return composite;
+		}
+
+		public string RemoveStudent()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ WCFStudent/WCFStudent/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WCFStudent
+{
+	[DataContract]
+	public class Student 
+	{
+		[DataMember]
+		public int Id { get; set; }
+		[DataMember]
+		public string Navn { get; set; }
+		[DataMember]
+		public string Efternavn { get; set; }
+		[DataMember]
+		public int Alder { get; set; }
+
+		public Student(int id, string navn, string efternavn, int alder)
+		{
+			this.Id = id;
+			this.Navn = navn;
+			this.Efternavn = efternavn;
+			this.Alder = alder;
+		}
+
+		
+
+		public override string ToString()
+		{
+			return $"Navn: {Navn}, Efternavn: {Efternavn}, Alder: {Alder}";
+		}
+	}
+}
